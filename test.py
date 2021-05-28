c494ba3d3dbb6c423f7fd91bedbd656099ddc20f
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-05-28 22:16:36 +0300

    adds imports to test file

--- test.py
+++ test.py
@@ -2,3 +2,8 @@ import unittest
 from credential_classes import Credentials
 from user_class import User
 
+class TestClass(unittest.TestCase):
+    '''
+    define test cases for user and credentials behavior
+    '''
+    
