f336cc120eb4115ea9f0f0a70ce2181e4f0ef3a6
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-05-28 22:25:17 +0300

    sets up a method that runs before each test case

--- test.py
+++ test.py
@@ -6,4 +6,12 @@ class TestClass(unittest.TestCase):
     '''
     define test cases for user and credentials behavior
     '''
+
+    def setUp(self):
+        '''
+        set up a method to run before each test cases
+        '''
+        self.new_user=("Caleb","oginga","caleb@ms.com","new21oginga")
+
+
     
