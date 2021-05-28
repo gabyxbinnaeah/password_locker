b241c053d1c75aba3cd2d0e26791600a851a36d4
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-05-28 21:59:30 +0300

    adds class that generates a new instance of a user

---
+++ user_class.py
@@ -0,0 +1,11 @@
+class User:
+    '''
+    generates a new instance of a user 
+    '''
+    userList=[]
+
+    def __init__(self,name,email_address,password):
+        '''
+        initialize properties
+        '''
+        
